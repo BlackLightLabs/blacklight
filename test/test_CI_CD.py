80bfb4980cdd7dccfba5c5b0aa166bf67414dd39
Cole Agard <coleagard@DESKTOP-FSR1RMM.localdomain>
2022-11-28 14:21:11 -0800

    [CI/CD] added test for CI/CD to run test. Should pass on github

---
+++ test/test_CI_CD.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestCICD(unittest.TestCase):
+    """
+    Basic test to assure that CI/CD is functioning properly
+    """
+    def test_base(self):
+        self.assertEqual(2, 2)
